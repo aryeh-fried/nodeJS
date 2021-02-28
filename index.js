51f125f5db7ef11235e5d902064da032de868d2f
aryeh-fried <aryeh.fried1@gmail.com>
2021-02-28 14:04:18 +0200

    added users_post

--- index.js
+++ index.js
@@ -1,134 +1,92 @@
 /*
-*Primary file for the API
-*
-*
-*/
+ * Primary file for API
+ *
+ */
 
 // Dependencies
-var http =  require('http');
-var url = require('url')
-var stringDecoder = require('string_decoder').StringDecoder;
-//var config = require( './config.js');
-var config = require('./config.js');// not working for some reason...
-var _data = require('./lib/data');
-var router = require('./router');
-//@TODO: delete this
-//TESTING
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./lib/config');
+var fs = require('fs');
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
-//testing creating a new json file
-
-// _data.create('test','newFile',{'foo':'bar'},(err)=>{
-//   console.log('this was the error', err);
-// });
-
-//testing reading data from a file
-
-// _data.read('test','newFile',(err,data)=>{
-//   console.log('the data is ',data);
-// });
-
-
-//testing updating the file
-
-// _data.update('test','newFile',{'boo':'far'},(err)=>{
-//    console.log('this was the error', err);
-//  });
-
-// //testing deleting a file
-
-// _data.delete('test','newFile',(err)=>{
-//     console.log('this was the error', err);
-//     });
-
-router.myRouter();
-
-// the server should respond to all requests with a string
-var server = http.createServer((req,res)=>{
-  // get the url and parse it
-  var parseUrl = url.parse(req.url,true);
-  // Get the path
-  var path = parseUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
-  //get the query string as an object
-  var queryStringObject = parseUrl.query;
-  //Get the http method
-  var method = req.method.toLowerCase();
-  //Get the header as an object
-  var headers = req.headers;
-  //Get the payload, if any
-  var decoder = new stringDecoder('utf-8');
-  var buffer='';
-  req.on('data',(data)=>{
-    buffer += decoder.write(data);
-  });
-  req.on('end',()=>{
-    buffer += decoder.end();
-    //choose the handler this request should go to
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath]: handlers.notFound
-
-    //construct the data object to send to the handler
-
-    var data = {
-      'trimmedPath': trimmedPath,
-      'queryStringObject': queryStringObject,
-      'method': method,
-      'headers':headers,
-      'payload':buffer
-    };
-
-    //route the request to the  handler specified in the router
-
-    chosenHandler('data',(statusCode,payload)=>{
-      //use the status code called back by the handler , or default
-      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-      //Ues the payload called back by the handler , or default to
-      payload = typeof(payload) == 'object' ? payload: {};
-      //convert the payload to a string
-      var payloadString = JSON.stringify(payload);
-
-      // return the response
-      //this line is for showing the payload receive in a json format
-      res.setHeader('content-Type','application/json')
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      console.log('returning this response :', statusCode, payloadString)
-
-    });
-
-  });
-
+ // Instantiate the HTTP server
+var httpServer = http.createServer(function(req,res){
+  unifiedServer(req,res);
 });
 
-// start the server and have it listen on port 3000
-server.listen(3000,()=>{
-  console.log("The server is listening  on port : 3000 now");
-})
-//define the handler
-var handlers = {};
+// Start the HTTP server
+httpServer.listen(config.httpPort,function(){
+  console.log('The HTTP server is running on port '+config.httpPort);
+});
 
-//sample handler
 
-handlers.sample = (data,callback)=>{
-  //callback a http status code and a payload object
-  callback(406,{'name': 'sample handler'});
+// All the server logic for both the http and https server
+var unifiedServer = function(req,res){
+
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
+
+  //Get the headers as an object
+  var headers = req.headers;
+
+  // Get the payload,if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+      buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+      buffer += decoder.end();
+
+      // Check the router for a matching path for a handler. If one is not found, use the notFound handler instead.
+      var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+      // Construct the data object to send to the handler
+      var data = {
+        'trimmedPath' : trimmedPath,
+        'queryStringObject' : queryStringObject,
+        'method' : method,
+        'headers' : headers,
+        'payload' : helpers.parseJsonToObject(buffer)
+      };
+
+      // Route the request to the handler specified in the router
+      chosenHandler(data,function(statusCode,payload){
+
+        // Use the status code returned from the handler, or set the default status code to 200
+        statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+
+        // Use the payload returned from the handler, or set the default payload to an empty object
+        payload = typeof(payload) == 'object'? payload : {};
+
+        // Convert the payload to a string
+        var payloadString = JSON.stringify(payload);
+
+        // Return the response
+        res.setHeader('Content-Type', 'application/json');
+        res.writeHead(statusCode);
+        res.end(payloadString);
+        console.log(trimmedPath,statusCode);
+      });
+
+  });
 };
-// ping handler
-handlers.ping = (data,callback)=>{
-  //callback a http status code and a payload object
-  callback(200,{'ping': 'ping'});
-};
 
-
-//not found handler
-
-handlers.notFound = (data,callback)=>{
-  callback(404);
-};
-//define a reqeust router
-
+// Define the request router
 var router = {
-  'sample': handlers.sample,
-  'ping':handlers.ping
-
+  'ping' : handlers.ping,
+  'users' : handlers.users
 };
